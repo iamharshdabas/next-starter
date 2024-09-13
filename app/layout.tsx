55327fdfe2ea1f106614dbb538be8c21e2349cc7
Harsh Dabas <iamharshdabas@gmail.com>
2024-09-13 19:45:46 +0530

    fix(layout): structure and styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,10 +32,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head />
       <body className={clsx("min-h-screen bg-background font-sans antialiased")}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex h-screen flex-col">
-            <div className="mx-auto w-full max-w-screen-2xl">
+          <div className="mx-auto max-w-screen-2xl">
+            <div className="relative flex h-screen flex-col">
               <Navbar />
-              <main className="px-8 py-16">{children}</main>
+              <main className="grow px-8 py-16">{children}</main>
             </div>
           </div>
         </Providers>
